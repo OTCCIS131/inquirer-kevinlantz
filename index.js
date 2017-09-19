572cbcf7a994c5a8839171f8e9ed2e4530555093
Unknown <kl0231582@otc.edu>
2017-09-18 22:42:48 -0500

    All questions added

--- index.js
+++ index.js
@@ -4,45 +4,66 @@ const chalk = require('chalk');
 
 
 console.log('Welcome To My Super Awesome Pizza Co')
-    // let questions = [size = "What size pizza?", cheese = "Extra cheese?", sauce = "What kind of sauce?", Toppings = "Which toppings?", cut = "How do you want it cut?"]
+    // let questions = [size = "What size pizza?", 
+    //cheese = "Extra cheese?", 
+    //sauce = "What kind of sauce?", Toppings = "Which toppings?", cut = "How do you want it cut?"]
 
 
-// let questions = {
 
-//     type: "list",
-//     name: "size ",
-//     message: "What size pizza? ",
-//     choices: ["Small", "Medium", "large"]
-
-// }
-
-// let quest = {
-//     type: "rawlist",
-//     name: "cheese",
-//     message: "Extra cheese? ",
-//     choices: ["Yes", "No"],
-//     when: "size",
-// }
-
-// inquirer.prompt(questions).then(function(answers) {})
+//inquirer.prompt(questions).then(function(answers) {})
 // inquirer.prompt(quest).then(function(answe) {})
 
+var questions = [{
+        type: "list",
+        name: "size ",
+        message: "What size pizza?",
+        choices: ["Small", "Medium", "Large"]
+    }, {
+        type: "list",
+        name: "cheese",
+        message: "Extra cheese?",
+        choices: ["Yes", "No"],
 
-inquirer.prompt([{
-    name: 'Size',
-    type: "list",
-    message: "What size pizza? ",
-    choices: ["Small", "Medium", "large"],
+    }, {
+        name: 'sauce',
+        message: 'What kind of sauce?',
+        type: 'list',
+        choices: ['Pizza Sauce', 'Alfredo Sauce', ],
+    },
+    {
+        name: 'toppings',
+        message: 'Which toppings?',
+        type: 'checkbox',
+        choices: ['Pepperoni', 'Mushroom', 'Sausage', 'Bacon', 'Green Peppers'],
+    },
+    {
+        name: 'cut',
+        message: 'How do you want the pizza cut?',
+        type: 'list',
+        choices: ['Triangles', 'Square', ],
+    }
+]
 
-}, {
-    name: 'Cheese',
-    type: "rawlist",
-    message: "Extra cheese? ",
-    choices: ["Yes", "No"],
+inquirer.prompt(questions).then(function(answers) {})
 
-}, {
-    name: 'Sauce',
-    type: "checkbox",
-    message: "Sauce? ",
-    choices: ["Yes", "No"]
-}], function(answers) {});
+
+
+// inquirer.prompt([{
+//     name: 'Size',
+//     type: "list",
+//     message: "What size pizza? ",
+//     choices: ["Small", "Medium", "large"],
+
+
+// }, {
+//     name: 'Cheese',
+//     type: "rawlist",
+//     message: "Extra cheese? ",
+//     choices: ["Yes", "No"],
+
+// }, {
+//     name: 'Sauce',
+//     type: "checkbox",
+//     message: "Sauce? ",
+//     choices: ["Yes", "No"]
+// }], function(answers) { });
